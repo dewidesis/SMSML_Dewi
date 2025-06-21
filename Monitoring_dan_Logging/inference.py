6ebf360df73389c1dd34e09775970be19bebdcfc
Dewi <130025955+dewidesis@users.noreply.github.com>
2025-06-21 12:52:21 +0700

    Add files via upload

---
+++ Monitoring_dan_Logging/inference.py
@@ -0,0 +1,15 @@
+import requests
+import json
+
+# URL untuk serving model
+URL = "http://localhost:5001/invocations"
+
+# Load data JSON
+with open("MLModel/serving_input_example.json") as f:
+    data = json.load(f)
+
+# Kirim request
+response = requests.post(URL, json=data)
+
+# Tampilkan hasil
+print("Prediction response:", response.text)
